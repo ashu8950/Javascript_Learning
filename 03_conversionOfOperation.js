5bfcfc811277265819d7cd043aed50bc127b30e2
Ashu <shabsingh961@gmail.com>
2024-12-30 17:36:25 +0530

    done

--- 03_conversionOfOperation.js
+++ 03_conversionOfOperation.js
@@ -1,13 +1,13 @@
-let score ='33abc' 
+// let score ='33abc' 
 
-console.log(typeof score);
+// console.log(typeof score);
 
 
-let inNumber = Number(score)
+// let inNumber = Number(score)
 
-console.log(typeof inNumber);
+// console.log(typeof inNumber);
 
-console.log(inNumber);
+// console.log(inNumber);
 
 
 //"33" => 33
@@ -15,14 +15,62 @@ console.log(inNumber);
 // true =>1
 //false => 0
 
-let isLoggedIn = 1 
+// let isLoggedIn = 1 
 
-let booleanIsLoggedIn  = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn  = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//**********************************Operations  **********************************/
+
+let val = 3
+let negVal = -val
+console.log(negVal);
+
+// console.log(2+1);
+// console.log(2-1);
+// console.log(2*1);
+// console.log(2**1);
+// console.log(2/1);
+// console.log(2%1);
+
+
+let str1 = "ashu"
+let str2 =" singh"
+
+let str3 =str1 + str2
+// console.log(str3);
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
+let num1 ,num2 , num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+// gameCounter++;
+console.log(gameCounter);
+++gameCounter
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
+
 
